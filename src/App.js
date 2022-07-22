7814530b0016ff2926776862b856177916c9e60a
del_afk <aramafkhamird@gmail.com>
2022-07-23 01:01:36 +0430

    all parts added - no js - no API

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 import Search from "./Search";
 import Result from './Result';
+import Gallery from './Gallery';
 
 function App() {
   return (
     <div className="App">
       <Search />
       <Result />
+      <Gallery />
     </div>
   );
 }
--- src/Search.js
+++ src/Search.js
@@ -1,16 +1,13 @@
 import React from "react";
 import "./Search.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-
 
 export default function Search(){
     return (
         <div className="search-wrapper">
       <h1>Dictionary</h1>
-      <form action="">
-        <input type="search" placeholder="search..."/>
-        <button type="submit"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+      <form class="searchbox" action="http://thecodeblock.com">
+        <input type="search" placeholder="Search the word here..." />
+        <button type="submit" value="search">&nbsp;</button>
     </form>
     </div>
     );
